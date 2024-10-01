eb617aaf7327a5ce63e7bd87fbc9812f7103ca0d
Nelson Malbone <nhmalbone@icloud.com>
2024-10-01 18:19:34 -0400

    initial commit had some varibles backwards but fixed now

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # take a list of words separated by commas and replace them with " "
 
-spam = []
+spam_list = []
 while True:
-    print("Enter a list of goods to get " + str(len(spam) + 1) + 'or enter nothing to stop')
+    print("Enter a list of goods to get " + str(len(spam_list) + 1) + ' or enter nothing to stop')
     name_list = input()
     if name_list == "":
         break
-    name_list = spam + [name_list]
+    spam_list = spam_list + [name_list]
 print('List to get: ')
-for name_list in spam:
+for name_list in spam_list:
     print(" " + name_list)
